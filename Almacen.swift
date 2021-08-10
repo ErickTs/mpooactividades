30038ce47f2a22b66f8a2582daff905406d7136c
Erick <42413099+ErickTs@users.noreply.github.com>
2021-08-10 07:14:52 +0000

    Almacen

---
+++ Almacen.swift
@@ -0,0 +1,29 @@
+struct Almacen{
+  var inventario: [(Producto, Int)]
+
+  init(){
+    inventario = []
+  }
+
+  func showProducts(by category: Category){
+    for producto in inventario{
+      if producto.0.isInCategory(category: category){
+        print(producto.0)
+      }
+    }
+  }
+
+  func showProducts(){
+    for producto in inventario{
+      print(producto.0, producto.1)
+    }
+  }
+
+  mutating func addProducto(producto: Producto, cantidad: Int){
+    var tupla = (producto, cantidad)
+    inventario.append(tupla)
+  }
+
+  mutating func removeProducts(producto: Producto, cantidad: Int){
+  }
+}
