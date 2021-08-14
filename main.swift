0ac6ea852503df7a95ddb3860e6c68f24e78ba7e
Erick <42413099+ErickTs@users.noreply.github.com>
2021-08-14 02:30:02 +0000

    Ordenamiento de lista

---
+++ main.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+func ordenamientoGg(miArreglo: inout [Int]) {
+    var n = miArreglo.count
+    while (n > 0) {
+        var j = 0
+        for i in 1..<n {
+            // Cambio del indice i
+            if miArreglo[i - 1] > miArreglo[i] {
+                let temp = miArreglo[i - 1]
+                miArreglo[i - 1] = miArreglo[i]
+                miArreglo[i] = temp
+                j = i
+            }
+        }
+       //inidice modificado
+        n = j
+    }
+}
+
+var miArreglo = [4,1,9,0,44,8,6,11,99]
+ordenamientoGg(miArreglo: &miArreglo)
+print(miArreglo)
